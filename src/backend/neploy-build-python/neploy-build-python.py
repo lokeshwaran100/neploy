9e5bfcd00e78c7cf250adc2696f31981514be6c0
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2023-08-26 20:39:58 +0530

    Integration

--- src/backend/neploy-build-python/neploy-build-python.py
+++ src/backend/neploy-build-python/neploy-build-python.py
@@ -18,7 +18,7 @@ def build_endpoint():
         with open(temp_filename, 'wb') as f:
             f.write(code.encode())
 
-        # Execute the code
+        # compile the code
         try:
             result = subprocess.run(["neo3-boa", "compile", temp_filename], capture_output=True, text=True)
             os.remove(temp_filename)  # Delete the temporary file
@@ -34,6 +34,43 @@ def build_endpoint():
                 response_data["message"] = "Build Failed"
             else:
                 response_data["message"] = "Build is Successful"
+            return jsonify(response_data), 200
+        except Exception as e:
+            os.remove(temp_filename)  # Delete the temporary file
+            return jsonify({"error": str(e)}), 500
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/optimise', methods=['POST'])
+def optimise_endpoint():
+    try:
+        data = request.json
+        if data is None or "code" not in data:
+            return jsonify({"error": "Invalid request data"}), 400
+
+        code = data["code"]
+        temp_filename = data["file"]
+
+        with open(temp_filename, 'wb') as f:
+            f.write(code.encode())
+
+        # optimise the code
+        try:
+            result = subprocess.run(["python3", "ml-code-optimiser.py", temp_filename], capture_output=True, text=True)
+            os.remove(temp_filename)  # Delete the temporary file
+
+            response_data = {
+                "stdout": result.stdout,
+                "stderr": result.stderr
+            }
+
+            if result.returncode != 0:
+                response_data["message"] = "Failed to optimise code"
+            elif 'ERROR' in result.stderr:
+                response_data["message"] = "Failed to optimise code"
+            else:
+                response_data["message"] = "Code is optmized successfully"
             return jsonify(response_data), 200
         except Exception as e:
             os.remove(temp_filename)  # Delete the temporary file
