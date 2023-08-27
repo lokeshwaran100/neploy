ace0938bc8d5c8a318bda51835583a08869d7580
Akshat Sachan <140371441+akshat-sachan@users.noreply.github.com>
2023-08-27 11:45:16 +0530

    :zap: ui

--- src/frontend/static/script.js
+++ src/frontend/static/script.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const compileButton = document.getElementById("compile-button");
     const editorTextArea = document.getElementById("editor");
     const optimizerButton = document.getElementById("optimizer-button");
+
     compileButton.addEventListener("click", function() {
         const code = editorTextArea.value;
 
-
         const data = { "code": code, "file": "code.py" };
-
 
         fetch('http://127.0.0.1:30410/build', {
             method: 'POST',
@@ -20,40 +19,31 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(responseData => {
             console.log("Server response:", responseData);
 
+            const outputBox = document.getElementById("output-box");
+            outputBox.textContent = `${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}\n`;
 
-            const outputArea = document.getElementById("output");
-            outputArea.textContent = `\n${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}`;
+            
+            fetch('http://127.0.0.1:30410/optimise', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(optimizedData => {
+                console.log("Optimizer response:", optimizedData);
+
+                outputBox.textContent += `\nOptimizer Message: ${optimizedData.message}\n\nOptimized Code:\n${optimizedData.result}`;
+            })
+            .catch(error => {
+                console.error('Error fetching optimizer data:', error);
+            });
         })
         .catch(error => {
             console.error('Error sending data to endpoint:', error);
         });
     });
-    optimizerButton.addEventListener("click", function() {
-        const code = editorTextArea.value;
 
-        const data = { "code": code, "file": "code.py" };
-
-        fetch('http://127.0.0.1:30410/optimise', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(responseData => {
-          console.log("Optimizer response:", responseData);
-
-
-          const outputArea = document.getElementById("output");
-          outputArea.textContent = `\n${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}`;
-        })
-        .catch(error => {
-          console.error('Error fetching optimizer data:', error);
-        });
-      });
-    });
-
-
-
-
+   
+});
