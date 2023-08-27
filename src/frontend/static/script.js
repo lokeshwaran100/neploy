9749219cf9b32e6189b23200ea3fc9a9456ca3af
Akshat Sachan <140371441+akshat-sachan@users.noreply.github.com>
2023-08-27 09:48:17 +0530

    :sparkles: ui optimizer

--- src/frontend/static/script.js
+++ src/frontend/static/script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const compileButton = document.getElementById("compile-button");
     const editorTextArea = document.getElementById("editor");
-
+    const optimizerButton = document.getElementById("optimizer-button");
     compileButton.addEventListener("click", function() {
         const code = editorTextArea.value;
 
-        // Create an object with the code and file name
+      
         const data = { "code": code, "file": "code.py" };
 
-        // Send code and file name to the specified endpoint URL
+       
         fetch('http://127.0.0.1:30410/build', {
             method: 'POST',
             headers: {
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(responseData => {
             console.log("Server response:", responseData);
 
-            // Update the output area with stdout and stderr values
+         
             const outputArea = document.getElementById("output");
             outputArea.textContent = `\n${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}`;
         })
@@ -28,4 +28,27 @@ document.addEventListener("DOMContentLoaded", function() {
             console.error('Error sending data to endpoint:', error);
         });
     });
-});
+    optimizerButton.addEventListener("click", function() {
+        fetch('http://127.0.0.1:30410/optimizer', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then(responseData => {
+          console.log("Optimizer response:", responseData);
+
+         
+          const outputArea = document.getElementById("output");
+          outputArea.textContent = `\n${responseData.message}\n\n${responseData.result}`;
+        })
+        .catch(error => {
+          console.error('Error fetching optimizer data:', error);
+        });
+      });
+    });
+
+
+
+
