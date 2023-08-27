e7408969895e581b33abe821b8437e424d1e8414
Lokeshwaran Boopathy <lokeshwaran.boopathy@oracle.com>
2023-08-27 10:09:34 +0530

    Updated optimise feature

--- src/frontend/static/script.js
+++ src/frontend/static/script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     compileButton.addEventListener("click", function() {
         const code = editorTextArea.value;
 
-      
+
         const data = { "code": code, "file": "code.py" };
 
-       
+
         fetch('http://127.0.0.1:30410/build', {
             method: 'POST',
             headers: {
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(responseData => {
             console.log("Server response:", responseData);
 
-         
+
             const outputArea = document.getElementById("output");
             outputArea.textContent = `\n${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}`;
         })
@@ -29,19 +29,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
     optimizerButton.addEventListener("click", function() {
-        fetch('http://127.0.0.1:30410/optimizer', {
-          method: 'GET',
+        const code = editorTextArea.value;
+
+        const data = { "code": code, "file": "code.py" };
+
+        fetch('http://127.0.0.1:30410/optimise', {
+          method: 'POST',
           headers: {
             'Content-Type': 'application/json'
-          }
+          },
+          body: JSON.stringify(data)
         })
         .then(response => response.json())
         .then(responseData => {
           console.log("Optimizer response:", responseData);
 
-         
+
           const outputArea = document.getElementById("output");
-          outputArea.textContent = `\n${responseData.message}\n\n${responseData.result}`;
+          outputArea.textContent = `\n${responseData.message}\n\n${responseData.stdout}\n\n${responseData.stderr}`;
         })
         .catch(error => {
           console.error('Error fetching optimizer data:', error);
